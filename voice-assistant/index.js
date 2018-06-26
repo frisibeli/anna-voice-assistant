993d94c64a2796f9031845333e289d0dddbbea07
Kristiyan Krumov <kristiyan@Kristiyans-MacBook-Pro.local>
2018-06-27 00:15:27 +0300

    voice-assistant

---
+++ voice-assistant/index.js
@@ -0,0 +1,14 @@
+const Sonus = require('sonus');
+const speech = require('@google-cloud/speech')({
+    projectId: 'voice-assistant',
+    keyFilename: './voice-assistant-d5964a2c2b89.json'
+})
+
+const hotwords = [{ file: 'resources/Anna.pmdl', hotword: 'snowboy' }]
+const language = "bg-BG";
+const sonus = Sonus.init({ hotwords, language }, speech)
+
+Sonus.start(sonus)
+sonus.on('hotword', (index, keyword) => console.log("!"))
+sonus.on('final-result', console.log)
+  
