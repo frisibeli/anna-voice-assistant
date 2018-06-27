95937b92b4f8db12bbb4cc44781fb8d501a491ca
Nickole Dimitrova <nickole.dimitrova@gmail.com>
2018-06-27 18:51:30 +0300

    added sound on hotword

--- voice-assistant/index.js
+++ voice-assistant/index.js
@@ -3,17 +3,24 @@ const speech = require('@google-cloud/speech')({
     projectId: 'voice-assistant',
     keyFilename: './config/voice-assistant-d5964a2c2b89.json'
 })
-const speak = require('./src/text-to-speech')
-const call = require('./src/event-handlers/make-call')
+const speak = require('./src/text-to-speech');
+const call = require('./src/event-handlers/make-call');
+
+const play = require('play');
+play.playerList = ['play'];
 
 const hotwords = [{ file: 'resources/Anna.pmdl', hotword: 'anna' }]
 const language = "bg-BG";
 const sonus = Sonus.init({ hotwords, language }, speech)
 
 Sonus.start(sonus)
-sonus.on('hotword', (index, keyword) => console.log("!"));
+sonus.on('hotword', (index, keyword) => {
+    console.log("!");
+    const wakeUpSound = __dirname + '/resources/wake-up-sound.mp3';
+    play.sound(wakeUpSound);
+});
 sonus.on('final-result', result => {
-    console.log(result)
+    console.log(result);
     speak(result);
     if (result.includes("стоп")) {
         Sonus.stop()
