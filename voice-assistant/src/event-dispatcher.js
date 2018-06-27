9ad81d31e27b0974008aac66dc99d232e8a973a0
Nickole Dimitrova <nickole.dimitrova@gmail.com>
2018-06-28 00:31:16 +0300

    news

--- voice-assistant/src/event-dispatcher.js
+++ voice-assistant/src/event-dispatcher.js
@@ -1,6 +1,7 @@
 const makeCall = require('./event-handlers/make-call');
 const getTime = require('./event-handlers/get-time');
 const {turnOnLamp, turnOffLamp} = require('./event-handlers/iot-handler');
+const getNews = require('./event-handlers/news-handler');
 const fallback= require('./event-handlers/fallback-handler');
 
 module.exports = event => {
@@ -10,6 +11,7 @@ module.exports = event => {
         case 'make_call': return makeCall(event);
         case 'turn_lamp_on': return turnOnLamp(event);
         case 'turn_lamp_off': return turnOffLamp(event);
+        case 'get_latest_news': return getNews.getLatestNews(event);
     }
     return fallback(event);
 }
